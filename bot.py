85f78d5bba82539260409f61f493013569cb876e
Blastmerder <100530405+Blastmerder@users.noreply.github.com>
2022-03-24 20:32:05 +0300

    вав

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ globalCtx = ""
 
 print(10/5)
 
-token = "OTM3NzA4NTgxNzUzNjAyMDQ4.YffrKw.JloFFdcRAMNc3nZ-S9XFXNcY2tM"
+token = "OTM3NzA4NTgxNzUzNjAyMDQ4.YffrKw.YUt3nLz7hMqqcgboZm50PM66-8Y"
 bot = commands.Bot(command_prefix="!", intents=intents)
 waitText = False
 commandWait = ""
